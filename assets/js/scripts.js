1ac335c8434efa94db456c5f0e0f19eb8f31af89
jukoor <93034760+jukoor@users.noreply.github.com>
2021-12-23 15:52:01 +0100

    Settings Menu Extended

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -27,7 +27,9 @@
 		currentWeather: find('.js_current_weather'),
 		setDayTodayBtn: find('.js_set_date_today'),
 		latLong: find('.js_lat_long'),
-		airquality: find('.js_air_quality .js_value')
+		airquality: find('.js_air_quality .js_value'),
+		settingsSave: find('.js_settings .js_save'),
+		settingsLocation: findId('locationSearch')
 	};
 
 	let weatherDataTemp;
@@ -144,7 +146,7 @@
 			.then(data => {
 				displayWeather(data, 0);
 				weatherDataTemp = data;
-				console.log("fetched");
+				// console.log("fetched");
 			}).catch(function(error) {
 				console.log(error);
 			});
@@ -199,8 +201,8 @@
 		fetch(airPollutionUrl)
 			.then(response => response.json())
 			.then(data => {
-				console.log(data);
-				elements.airquality.textContent = ratings[data.list[0].main.aqi-1];
+				// console.log(data);
+				elements.airquality.textContent = ratings[data.list[0].main.aqi - 1];
 				// console.log()
 			})
 			.catch(() => {
@@ -347,5 +349,38 @@
 			find('.js_today_label').textContent = 'Today';
 			displayWeather(weatherDataTemp, 0);
 		});
+
+		/* Settings: Save */
+		elements.settingsSave.addEventListener('click', function() {
+			this.classList.add('active');
+		});
+
+		// city search
+		let citySearchApi;
+		let citySearchLength = 0;
+
+
+
+		var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
+
+		elements.settingsLocation.addEventListener('keyup', function() {
+			let val = this.value;
+			// console.log(this.value);
+			// citySearchLength++;
+			citySearchApi = `http://api.openweathermap.org/geo/1.0/direct?q=${this.value}&limit=5&appid=${weatherApiKey}`;
+
+			// City Search
+			fetch(citySearchApi)
+				.then(response => response.json())
+				.then(data => {
+					console.log(data);
+					autocomplete(elements.settingsLocation, data, val);
+				})
+				.catch(() => {
+				});
+		});
+
 	}
+
 })();
